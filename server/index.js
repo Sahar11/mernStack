4a5c69c889138f86967349dd0dfd623f10640659
Sahar <saharhalim151@gmail.com>
2023-11-20 15:24:24 -0500

    add mongodb credentials

--- server/index.js
+++ server/index.js
@@ -1,9 +1,24 @@
 import express from "express";
 import cors from "cors";
-import mongoose from mongoose
-const app = express()
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
+
+
+const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(3001, () => console.log("Server Started!"))
+app.get('/', (request, response) => {
+  return response.status(234).send("Welcome to MERN Stack Tutorial")
+})
+
+
+mongoose
+.connect(mongoDBURL)
+.then(() => {
+  app.listen(PORT, () => console.log(`Server Started at Port ${PORT}!`)); 
+})
+.catch((error) => {
+console.log(error)
+})
